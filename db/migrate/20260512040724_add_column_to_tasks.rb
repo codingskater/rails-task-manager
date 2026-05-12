7fa9e1fe91ba9ba8c03043e426f5092d48474b7a
Anna Costello <271207945+codingskater@users.noreply.github.com>
2026-05-12 13:22:59 +0900

    created the model and migrated

---
+++ db/migrate/20260512040724_add_column_to_tasks.rb
@@ -0,0 +1,4 @@
+class AddColumnToTasks < ActiveRecord::Migration[8.1]
+  def change
+  end
+end
---
+++ db/migrate/20260512041454_change_default_completed_in_tasks.rb
@@ -0,0 +1,5 @@
+class ChangeDefaultCompletedInTasks < ActiveRecord::Migration[8.1]
+  def change
+    change_column_default :tasks, :completed, false
+  end
+end
